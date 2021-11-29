26580d37377017024e0f599839cdb8640d2dbf23
lisa <lisa.marie.lull@gmail.com>
2021-11-29 09:39:38 -0500

    fixed

--- src/components/ShoutOutItem.tsx
+++ src/components/ShoutOutItem.tsx
@@ -1,6 +1,4 @@
-import { useContext } from "react";
 import { Link } from "react-router-dom";
-import AuthContext from "../context/AuthContext";
 import ShoutOut from "../models/ShoutOut";
 import "./ShoutOutItem.css";
 
